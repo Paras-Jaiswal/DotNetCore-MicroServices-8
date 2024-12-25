943f53229892716ec541ff95a909e8b75fc5bef2
ParasJaiswal <parasjaiswal630@gmail.com>
2024-12-25 21:25:17 +0530

    Order context and structure created

---
+++ Ecommerce/Services/Ordering/Ordering.Infrastructure/Data/OrderContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Core.Common;
+using Ordering.Core.Entities;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options) { }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public override Task<int> SaveChangesAsync(bool acceptAllChnagesOnSuccess, CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedDate = DateTime.Now;
+                        entry.Entity.CreatedBy = "rahul"; //TODO: replace with auth server
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.LastModifiedDate = DateTime.Now;
+                        entry.Entity.LastModifiedBy = "rahul"; //TODO: replace with auth server
+                        break;
+                }
+            }
+            return base.SaveChangesAsync(acceptAllChnagesOnSuccess, cancellationToken);
+        }
+    }
+}
---
+++ Ecommerce/Services/Ordering/Ordering.Infrastructure/Data/OrderContextSeed.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using Ordering.Core.Entities;
+
+namespace Ordering.Infrastructure.Data
+{
+    public class OrderContextSeed
+    {
+        public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContext> logger)
+        {
+            if (!orderContext.Orders.Any())
+            {
+                //orderContext.Orders.AddRange(Ge)
+            }
+        }
+
+        private static IEnumerable<Order> GetOrders()
+        {
+            return new List<Order> {
+                new()
+                {
+                    UserName = "rahul",
+                    FirstName = "Rahul",
+                    LastName = "Sahay",
+                    EmailAddress = "rahulsahay@eCommerce.net",
+                    AddressLine = "Bangalore",
+                    Country = "India",
+                    TotalPrice = 750,
+                    State = "KA",
+                    ZipCode = "560001",
+
+                    CardName = "Visa",
+                    CardNumber = "1234567890123456",
+                    CreatedBy = "Rahul",
+                    Expiration = "12/25",
+                    Cvv = "123",
+                    PaymentMethod = 1,
+                    LastModifiedBy = "Rahul",
+                    LastModifiedDate = new DateTime(),
+                }
+            };
+        }
+    }
+}
